e75a8b7a35757a52ebce054bbdab42fa36b1a01e
尤雨东 <root@ltpp.vip>
2025-02-11 21:29:32 +0800

    feat: v0.2.0

--- src/config/type.rs
+++ src/config/type.rs
@@ -9,7 +9,7 @@
 /// This structure holds the configuration values including the PID file path, stdout log path, and stderr log path.
 #[allow(dead_code)]
 pub struct ServerConfig {
-    pub(crate) pid_file: String,
-    pub(crate) stdout_log: String,
-    pub(crate) stderr_log: String,
+    pub pid_file: String,
+    pub stdout_log: String,
+    pub stderr_log: String,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ pub(crate) mod cfg;
 pub(crate) mod config;
 pub(crate) mod manager;
 
-pub(crate) use config::r#type::ServerConfig;
 #[cfg(unix)]
 pub(crate) use daemonize::Daemonize;
 pub(crate) use std::{error::Error, fs, process};
 
-pub use manager::r#type::ServerManager;
+pub use config::r#type::*;
+pub use manager::r#type::*;
