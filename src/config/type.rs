c5e23894d088d7b6b95f002301d408bbfb749f05
尤雨东 <root@ltpp.vip>
2025-02-12 22:06:51 +0800

    feat: v3.0.6

--- src/config/type.rs
+++ src/config/type.rs
@@ -6,7 +6,7 @@
 /// Returns:
 /// - None
 ///
-/// This structure holds the configuration values including the PID file path, stdout log path, and stderr log path.
+/// This structure holds the configuration values including the PID file path.
 #[allow(dead_code)]
 pub struct ServerManagerConfig {
     pub pid_file: String,
